1d9e8e76f65c7c5e25c525b4eedcfde349e3dade
joelbarba <joel.barba.vidal@gmail.com>
2015-03-22 20:20:48 +0100

    $comentari

--- github_ok.sh
+++ github_ok.sh
@@ -1,4 +1,15 @@
 echo "Procediment per commitar canvis al repositori local i exportar-los a GitHub :"
+echo "Selecciona la branca correcte (C = casa / O = oficina) :"
+read x
+if [ $x == "O" ]; then
+	branca="oficina"
+else
+	branca="casa"
+fi
+
+echo "--> GIT CHECKOUT $branca"
+git checkout $branca
+
 echo ""
 echo "--> GIT STATUS (estat dels fitxers modificats)"
 git status
@@ -14,8 +25,29 @@ git status
 echo "--> GIT COMMIT : introduir el comentari"
 read comentari
 git commit -m '$comentari'
-echo "--> GIT PUSH github master (exportar branca master a GitHub) (usr: joelbarba pwd: cocaina02)"
+
+echo "Exportar branca $branca a GITHUB"
+echo "--> GIT PUSH github $branca (usr=joelbarba, pwd=cocaina02)"
+read x
+git push github $branca
+
+echo "Avançar branca MASTER a $branca"
+echo "--> GIT CHECKOUT MASTER + GIT MERGE $branca"
+read x
+git checkout master
+git merge $branca
+
+echo "Exportar branca MASTER a GITHUB"
+echo "--> GIT PUSH github master (usr=joelbarba, pwd=cocaina02)"
 read x
 git push github master
+
+echo "Tornar a la branca de treball ($branca)"
+read x
+git checkout $branca
+
+echo "Final"
+exit 0;
+
 
 
